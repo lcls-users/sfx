c1dbd6aa412dcc88149929c9dd034f5e192e290f
Amke Nimmrich <50399391+AmkeNimm@users.noreply.github.com>
2022-02-25 14:22:35 -0800

    bug fixing

--- sfx_utils/timetool/TT_functions.py
+++ sfx_utils/timetool/TT_functions.py
@@ -188,7 +188,7 @@ def get_delay(run_start, run_end,
             stamp = np.append(stamp, str(sec) + "-" + str(nsec) + "-" + str(fid))
             if not redoTT:
                 edge_pos = np.append(edge_pos, ds.env().epicsStore().value(f'{beamline}:TIMETOOL:FLTPOS'))
-                if not model:
+                if len(calib_model) == 0:
                     tt_delay = np.append(tt_delay, ds.env().epicsStore().value(f'{beamline}:TIMETOOL:FLTPOS_PS')*1000)
                 else:
                     tt_delay = np.append(tt_delay, rel_time(edge_pos[-1], calib_model))
