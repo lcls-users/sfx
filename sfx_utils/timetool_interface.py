b42462128a23b7b94b2f4c7d18a1817ecebb1864
fredericpoitevin <frederic.poitevin@gmail.com>
2022-02-26 18:00:56 -0800

    Added Class to convert a dictionary (read from yml file) to a class.

--- sfx_utils/timetool_interface.py
+++ sfx_utils/timetool_interface.py
@@ -3,12 +3,26 @@ from matplotlib import pyplot as plt
 
 from psana_interface import *
 
+class AttrDict(dict):
+    """Class to convert a dictionary to a class.
+
+    Parameters
+    ----------
+    dict: dictionary
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Return a class with attributes equal to the input dictionary."""
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
 class TimetoolInterface:
 
     def __init__(self, path_config):
 
         with open(path_config, "r") as config_file:
-            self.config = yaml.safe_load(config_file)
+            self.config = AttrDict(yaml.safe_load(config_file))
 
         self.init_calib_data()
 
