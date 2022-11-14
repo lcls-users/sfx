75ba2b8f0fdd2954bfc67690fccd1f246732dd17
Ariana Peck <apeck@pslogin02.slac.stanford.edu>
2022-11-14 14:45:55 -0800

    fileinput attempted fix

--- btx/interfaces/ischeduler.py
+++ btx/interfaces/ischeduler.py
@@ -82,7 +82,8 @@ class JobScheduler:
         """ Write application and source requested dependencies. """
         with open(self.jobfile, 'a') as jfile:
             jfile.write(application)
-        for line in fileinput.input([self.jobfile], inplace=True):
+        for line in fileinput.FileInput([self.jobfile], inplace=True):
+        #for line in fileinput.input([self.jobfile], inplace=True):
             if line[0] != "#" and line[0] != " " and line != "\n":
                 sys.stdout.write(f"srun -n {self.ncores} shifter --image={os.environ['BTX_IMAGE']} --entrypoint {line}")
             else:
