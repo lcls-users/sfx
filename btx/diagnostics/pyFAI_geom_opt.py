b3d3346c840c51d2e2c1b3830d5106b4381fe125
LouConreux <louis.conreux1@gmail.com>
2024-04-22 13:28:27 -0700

    [edit] Change opti tolerance

--- btx/diagnostics/pyFAI_geom_opt.py
+++ btx/diagnostics/pyFAI_geom_opt.py
@@ -123,7 +123,7 @@ class pyFAI_Geometry_Optimization:
                 sg.geometry_refinement.param[1],
                 sg.geometry_refinement.param[2],
             ]
-            if np.allclose(params, new_params, atol=pixel_size):
+            if np.allclose(params, new_params, atol=pixel_size // 2):
                 print(f"Converged after {r} iterations")
                 return new_params
             else:
