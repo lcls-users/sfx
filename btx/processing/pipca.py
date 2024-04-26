d54a9dc6030453d4728f881b15f47c605e8bc6d4
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-04-26 10:20:35 -0700

    Test numpy 3.0

--- btx/processing/pipca.py
+++ btx/processing/pipca.py
@@ -271,7 +271,7 @@ class PiPCA:
         num_valid_imgs, p, x, y = imgs.shape
         formatted_imgs = np.reshape(imgs, (num_valid_imgs, p * x * y)).T
 
-        formatted_imgs_to_scatter = np.split(formatted_imgs, self.split_indices)
+        formatted_imgs_to_scatter = np.split(formatted_imgs, self.split_indices[1:])
 
         #for i in range(0,self.comm.Get_size()):
         #    formatted_imgs_to_scatter.append(formatted_imgs[self.split_indices[i]:self.split_indices[i + 1],:])
