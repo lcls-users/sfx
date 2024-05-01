57b4912814f8d3ecff20cbaf4ad4010bec4e9fea
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-05-01 13:11:01 -0700

    Test Sklearn IPCA 3.0

--- btx/misc/pipca_visuals.py
+++ btx/misc/pipca_visuals.py
@@ -474,7 +474,7 @@ def sklearn_ipca_test(filename, num_components, batch_size):
     list_eigenimages_ipca = []
     ipca = IncrementalPCA(n_components=num_components, batch_size=batch_size)
     for i in range(0, len(imgs), batch_size):
-        print(f"Processing batch {i} of {len(imgs)}")
+        print(f"Processing batch {int(i/batch_size)+1} of {int(num_images/batch_size)}")
         ipca.partial_fit(imgs[i:i+batch_size].reshape(num_images, -1))
         print(f"Batch {i} processed")
     eigenimages_ipca = ipca.components_
