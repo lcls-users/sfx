87698bf6872921fb1fd2024d03b47eb1163bfad7
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-05-06 14:31:46 -0700

    Working display

--- btx/misc/pipca_visuals.py
+++ btx/misc/pipca_visuals.py
@@ -152,46 +152,30 @@ def display_dashboard_pytorch(filename):
         img = psi.get_images(1)
         img = img.squeeze()
 
-        list_loss = []
-        #Get reconstructed images
+        #Get reconstructed image
         first_compo = int(pcscree[2:])
         last_compo = int(pcscree2[2:])
+        img_reconstructed = np.dot(reconstructed_images[:, first_compo-1:last_compo], V[:, first_compo-1:last_compo].T)[img_source]+mu
+        img_reconstructed = img_reconstructed.reshape((p, x, y))
+        img_reconstructed = assemble_image_stack_batch(img_reconstructed, pixel_index_map)
 
-        for f in range(first_compo, last_compo+1):
-            img_reconstructed = np.dot(reconstructed_images[:, f-1:last_compo], V[:, f-1:last_compo].T)[img_source]+mu
-            img_reconstructed = img_reconstructed.reshape((p, x, y))
-            img_reconstructed = assemble_image_stack_batch(img_reconstructed, pixel_index_map)
-
-            #Compute loss
-            diff = np.abs(img - img_reconstructed)
-            loss = np.linalg.norm(diff, 'fro')
-            list_loss.append(loss)
-
-        list_loss = np.array(list_loss)
-        list_loss *= 100/np.linalg.norm(img, 'fro')
-        print(list_loss)
-
-        #Plot the losses for the selected image
-        losses = list_loss.reshape((last_compo-first_compo+1,1))
-        losses = np.concatenate((np.arange(first_compo,last_compo+1).reshape((last_compo-first_compo+1,1)), losses), axis=1)
-
-        opts = dict(width=800, height=300, show_grid=True, show_legend=False,
-                    shared_axes=False, toolbar='above', default_tools=['hover','save','reset'],
-                    xlabel='Number of components', ylabel='Loss')
-        scree = hv.Bars(losses, label="Loss Plot").opts(**opts)
-
-        return scree
+        #Compute loss
+        diff = np.abs(img - img_reconstructed)
+        loss = np.linalg.norm(diff, 'fro') / np.linalg.norm(img, 'fro') * 100
+        print(f"Loss: {loss:.2f}%")
+        return loss
 
     # Connect the Tap stream to the heatmap callbacks
     stream1 = [posxy]
     stream2 = Params.from_params({'pcx': PCx.param.value, 'pcy': PCy.param.value, 'pcscree': PC_scree.param.value, 'pcscree2': PC_scree2.param.value})
     tap_dmap = hv.DynamicMap(tap_heatmap, streams=stream1+stream2)
     tap_dmap_reconstruct = hv.DynamicMap(tap_heatmap_reconstruct, streams=stream1+stream2)
-    tap_dmap_loss = hv.DynamicMap(compute_loss, streams=stream1+stream2)
 
+    PC_scree.param.watch(lambda event: compute_loss(posxy.x, posxy.y,PCx.value, PCy.value, PC_scree.value, PC_scree2.value), 'value')
+    PC_scree2.param.watch(lambda event: compute_loss(posxy.x, posxy.y,PCx.value, PCy.value, PC_scree.value, PC_scree2.value), 'value')
+    
     return pn.Column(pn.Row(widgets_scatter, create_scatter, tap_dmap),
-                     pn.Row(widgets_scree, create_scree, tap_dmap_reconstruct),
-                     pn.Row(widgets_scree, tap_dmap_loss)).servable('PiPCA Dashboard')
+                     pn.Row(widgets_scree, create_scree, tap_dmap_reconstruct)).servable('PiPCA Dashboard')
 
 def display_dashboard(filename):
     """
