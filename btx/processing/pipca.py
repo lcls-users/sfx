3fe58a4d0696a02290371fed64fc778983d94f8d
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-05-21 18:48:08 -0700

    Test 16.0

--- btx/processing/pipca.py
+++ btx/processing/pipca.py
@@ -1038,9 +1038,7 @@ class iPCA_Pytorch:
             reconstructed_batch = ipca._validate_data(batch_imgs.reshape(end-start, -1))
             reconstructed_batch = ipca.transform(reconstructed_batch)
             reconstructed_batch = reconstructed_batch.cpu().detach().numpy()
-            print("BATCH : ",reconstructed_batch.shape)
             reconstructed_images = np.concatenate((reconstructed_images, reconstructed_batch), axis=0)
-            print("IMAGES : ",reconstructed_images.shape)
 
         logging.info("Images reconstructed")
 
