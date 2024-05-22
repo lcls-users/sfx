7571e35c56114c5abd2abbc21537425c5b46317e
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-05-22 10:48:59 -0700

    Test with nn.DataParallel 4.0

--- btx/processing/pipca.py
+++ btx/processing/pipca.py
@@ -1022,6 +1022,7 @@ class iPCA_Pytorch:
         with TaskTimer(self.task_durations, "Initializing model"):
             ipca = IncrementalPCAonGPU(n_components = self.num_components, batch_size = self.batch_size)
             ipca = nn.DataParallel(ipca)
+            ipca = ipca.module
 
         logging.info("Images loaded and formatted and model initialized")
 
