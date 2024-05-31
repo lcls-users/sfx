02f095374a2fe45c6c6cbd5bdc332a98252a4280
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-05-31 14:49:13 -0700

    It worked ! Test acknowledgment

--- btx/interfaces/iclient.py
+++ btx/interfaces/iclient.py
@@ -94,5 +94,6 @@ if __name__ == "__main__":
     print('Letsgo')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect(server_address)
+        socket.sendall("ACK".encode('utf-8'))
         sock.sendall("DONE".encode('utf-8'))
 
