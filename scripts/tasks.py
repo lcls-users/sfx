65d1a6a21e3bbc4fed195fb45411b912569780e8
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-06-02 00:15:53 -0700

    Test main pipca 5.0

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -785,7 +785,7 @@ def test_serv_client(config):
     command += "; conda activate ana-4.0.59-py3; echo 'Client environment activated'"
     command += f"; python {client_path} -e {exp} -r {run} -d {det_type} --start_offset {start_offset} --num_images {num_images} --loading_batch_size {loading_batch_size} --num_components {num_components} --batch_size {batch_size} --path {path} --tag {tag}"
 
-    js = JobScheduler(os.path.join(".", f'test_serv_client.sh'),queue = 'milano', ncores=1, jobname=f'test_serv_client',logdir='/sdf/home/n/nathfrn/btx/scripts')
+    js = JobScheduler(os.path.join(".", f'test_serv_client.sh'),queue = 'ampere', ncores=1, jobname=f'test_serv_client',logdir='/sdf/home/n/nathfrn/btx/scripts')
     js.write_header()
     js.write_main(f"{command}\n", dependencies=['psana'])
     js.clean_up()
