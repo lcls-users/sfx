00a7f0f6c64d4db9088f8e7268a042c5a926c2fd
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-06-03 13:56:59 -0700

    Test

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -783,7 +783,7 @@ def test_serv_client(config):
     command += "; sleep 10"
     command += ";conda deactivate; echo 'Server environment deactivated'"
     command += "; conda activate /sdf/home/n/nathfrn/mfxp23120/scratch/nathfrn/env_nath; conda list; echo 'Client environment activated'"
-    command += f"; conda run -p /sdf/home/n/nathfrn/mfxp23120/scratch/nathfrn/env_nath python {client_path} -e {exp} -r {run} -d {det_type} --start_offset {start_offset} --num_images {num_images} --loading_batch_size {loading_batch_size} --num_components {num_components} --batch_size {batch_size} --path {path} --tag {tag}"
+    command += f"; /sdf/home/n/nathfrn/mfxp23120/scratch/nathfrn/env_nath/bin/python {client_path} -e {exp} -r {run} -d {det_type} --start_offset {start_offset} --num_images {num_images} --loading_batch_size {loading_batch_size} --num_components {num_components} --batch_size {batch_size} --path {path} --tag {tag}"
 
     js = JobScheduler(os.path.join(".", f'test_serv_client.sh'),queue = 'ampere', ncores=1, jobname=f'test_serv_client',logdir='/sdf/home/n/nathfrn/btx/scripts')
     js.write_header()
