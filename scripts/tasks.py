1d922027e72983007f915e4505dec027ac5c0cb0
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-06-03 15:36:50 -0700

    Test

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -786,7 +786,7 @@ def test_serv_client(config):
     command += "; sleep 10"
     command += ";conda deactivate; echo 'Server environment deactivated'"
     command += "; conda activate /sdf/home/n/nathfrn/mfxp23120/scratch/nathfrn/env_nath; which python; echo 'Client environment activated'"
-    command += f"; mpirun -n {ncores} --mca opal_cuda_support 1 python {client_path} -e {exp} -r {run} -d {det_type} --start_offset {start_offset} --num_images {num_images} --loading_batch_size {loading_batch_size} --num_components {num_components} --batch_size {batch_size} --path {path} --tag {tag}"
+    command += f"; mpirun -n {ncores} python {client_path} -e {exp} -r {run} -d {det_type} --start_offset {start_offset} --num_images {num_images} --loading_batch_size {loading_batch_size} --num_components {num_components} --batch_size {batch_size} --path {path} --tag {tag}"
 
     js = JobScheduler(os.path.join(".", f'test_serv_client.sh'),queue = 'ampere', ncores=1, jobname=f'test_serv_client',logdir='/sdf/home/n/nathfrn/btx/scripts')
     js.write_header()
