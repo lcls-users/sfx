cccf81ade283ceb24dfaf8acae922e4c5eaa5640
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-06-04 00:07:50 -0700

    Test

--- btx/processing/PCAonGPU/gpu_pca/pca_module.py
+++ btx/processing/PCAonGPU/gpu_pca/pca_module.py
@@ -311,13 +311,13 @@ class IncrementalPCAonGPU():
     def setup_rmm_pool(self):
         client = self.client
         client.run(
-            cupy.cuda.set_allocator,
+            cp.cuda.set_allocator,
             pool=True,
             initial_pool_size= parse_bytes("26GB"),
             allocator="default"
         )
         client.run(
-            cupy.cuda.set_allocator,
+            cp.cuda.set_allocator,
             rmm.rmm_cupy_allocator
         )
     
