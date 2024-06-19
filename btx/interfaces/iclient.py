9e1b90082a9b26fcdc1f07ce65b4234aa9bf061b
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-06-19 15:31:22 -0700

    Test handling bugged images

--- btx/interfaces/iclient.py
+++ btx/interfaces/iclient.py
@@ -60,8 +60,7 @@ class IPCRemotePsanaDataset(Dataset):
 
             except json.JSONDecodeError as e:
                 print("JSON decoding failed:", e)
-                response_data = '{"name": "dummy", "shape": [16,352,384], "dtype": "float32"}'
-                response_json = json.loads(response_data)
+                return np.zeros((16,352,384))
 
             # Use the JSON data to access the shared memory
             shm_name = response_json['name']
