ee900305266f9d53b24a0fe0f718ef6e03278c97
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-06-19 12:17:35 -0700

    It all worked, trying with max_events

--- btx/misc/get_max_events.py
+++ btx/misc/get_max_events.py
@@ -1,49 +1,16 @@
 from btx.interfaces.ipsana import PsanaImg
 
-def parse_input():
-    """
-    Parse command line input.
-    """
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--exp", help="Experiment name.", required=True, type=str)
-    parser.add_argument("-r", "--run", help="Run number.", required=True, type=int)
-    parser.add_argument(
-        "-d",
-        "--det_type",
-        help="Detector name, e.g epix10k2M or jungfrau4M.",
-        required=True,
-        type=str,
-    )
-    parser.add_argument(
-        "--start_offset",
-        help="Run index of first image to be incorporated into iPCA model.",
-        required=False,
-        type=int,
-    )
-    parser.add_argument(
-        "--num_images",
-        help="Total number of images to be incorporated into model.",
-        required=True,
-        type=int,
-    )
-
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    # Parse input arguments
-    args = parse_input()
-
+def main(exp,run,det_type,num_images):
     # Create PsanaImg object
-    psana_img = PsanaImg(args.exp, args.run, 'idx', args.det_type)
+    psana_img = PsanaImg(exp, run, 'idx', det_type)
 
     # Get maximum number of events
     max_events = psana_img.__len__()
     print(f"Maximum number of events: {max_events}")
     
-    if max_events < args.num_images:
+    if max_events < num_images:
         print("Number of images requested exceeds the maximum number of events.")
         return max_events
     
-    return args.num_images
+    return num_images
 
--- scripts/tasks.py
+++ scripts/tasks.py
@@ -758,6 +758,7 @@ def ipca_pytorch(config):
 def test_serv_client(config):
     print("TESTING SERVER AND CLIENT COMMUNICATION")
     from btx.interfaces.ischeduler import JobScheduler
+    from btx.misc.get_max_events import main
 
     setup = config.setup
     task = config.test_serv_client
@@ -766,6 +767,7 @@ def test_serv_client(config):
     det_type = setup.det_type
     start_offset = task.start_offset
     num_images = task.num_images
+    num_images = main(exp, run, det_type, start_offset, num_images)
     num_components = task.num_components
     batch_size = task.batch_size
     path = task.path
@@ -780,11 +782,9 @@ def test_serv_client(config):
 
     server_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),  "../btx/interfaces/iserver.py")
     client_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),  "../btx/interfaces/iclient.py")
-    get_max_events_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),  "../btx/misc/get_max_events.py")
 
     command = "which python;"
     command += f"python {server_path} & echo 'Server is running'"
-    command += f"; num_images = $(python ${get_max_events_path} -e {exp} -r {run} -d {det_type} --start_offset {start_offset} --num_images {num_images})"
     command += "; sleep 10"
     command += ";conda deactivate; echo 'Server environment deactivated'"
     command += "; conda activate /sdf/group/lcls/ds/tools/conda_envs/env_nath; which python; echo 'Client environment activated'"
