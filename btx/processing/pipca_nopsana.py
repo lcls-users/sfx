2d1dd76e010db6f9c61a5379c227062940b9be77
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-06-18 19:02:35 -0700

    Test with compression loss inside class

--- btx/processing/pipca_nopsana.py
+++ btx/processing/pipca_nopsana.py
@@ -77,7 +77,7 @@ class iPCA_Pytorch_without_Psana:
         logging.info("Images loaded and formatted and model initialized")
 
         with TaskTimer(self.task_durations, "Fitting model"):
-            ipca.fit(self.images.reshape(self.num_images, -1)[:self.num_training_images])
+            ipca.fit(self.images.reshape(self.num_images, -1)[:self.num_training_images-1])
     
         logging.info(f"Model fitted on {self.num_training_images} images")
 
@@ -106,7 +106,7 @@ class iPCA_Pytorch_without_Psana:
                     batch_imgs = self.images[start:end]
                     average_training_loss= ipca.compute_loss_pytorch(batch_imgs.reshape(end-start, -1))
                     average_training_losses.append(average_training_loss.cpu().detach().numpy())
-                    logging.info(f"Blablabl: {average_training_loss.cpu().detach().numpy()}")
+                    logging.info(f"Blablabla: {average_training_loss.cpu().detach().numpy()}")
                 average_training_loss = np.mean(average_training_losses)
                 logging.info(f"Average training loss: {average_training_loss*100:.3f} (in %)")
                 average_evaluation_losses = []
