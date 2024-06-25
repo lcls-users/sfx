84cdac9d047c56bc91f4329dea90c02564125335
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-06-25 12:18:29 -0700

    Test with dask cluster

--- btx/processing/PCAonGPU/gpu_pca/pca_module.py
+++ btx/processing/PCAonGPU/gpu_pca/pca_module.py
@@ -14,6 +14,7 @@ import cuml
 from cuml.dask.decomposition import TruncatedSVD
 from dask_cuda import LocalCUDACluster
 from dask.distributed import Client
+import cudf
 
 class IncrementalPCAonGPU():
     """
