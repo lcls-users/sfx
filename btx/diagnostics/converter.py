ab68e9639ec6add5e95e1f2e0e0e26713356a1c5
LouConreux <louis.conreux1@gmail.com>
2024-07-22 03:16:47 -0700

    [edit] Fix getting pixel coordinates from .data file and zeroing z component

--- btx/diagnostics/converter.py
+++ btx/diagnostics/converter.py
@@ -331,9 +331,7 @@ class CrystFELtoPyFAI:
             top = geo.get_top_geo()
             children = top.get_list_of_children()[0]
             pix_arr = geo.get_pixel_coords(oname=children.oname, oindex=0, do_tilt=True, cframe=CFRAME_PSANA)
-            print(np.shape(pix_arr[0]))
-            mean_z = np.mean(pix_arr[2])
-            pix_arr[:, :, :, 2] -= mean_z
+            pix_arr[2] -= np.mean(pix_arr[2])
         return pix_arr
 
     def get_corner_array(self, pix_pos, panels, cframe=CFRAME_PSANA):
