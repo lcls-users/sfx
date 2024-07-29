cc80d0c6a02f835f3219891be95db708103e7ae4
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-07-29 18:11:47 +0200

    Everything worked, TEST UNIQUE POOL

--- btx/interfaces/iclient.py
+++ btx/interfaces/iclient.py
@@ -271,7 +271,7 @@ if __name__ == "__main__":
                 all_data.append(batch)
                 current_loading_batch.append(batch)
             print(f"Loaded {event+loading_batch_size} images.")
-
+            current_loading_batch = np.concatenate(current_loading_batch, axis=0)
             current_loading_batch = current_loading_batch[[i for i in range(loading_batch_size) if not np.isnan(current_loading_batch[i : i + 1]).any()]]
 
             logging.info(f"Number of non-none images: {current_loading_batch.shape[0]}")
