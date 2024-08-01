8dbfde97e6af39b7ac65b30c8b6bf4f63505fa17
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-08-01 16:26:45 +0200

    Test

--- btx/processing/pipca_nopsana.py
+++ btx/processing/pipca_nopsana.py
@@ -299,7 +299,6 @@ class iPCA_Pytorch_without_Psana:
         self.device = device
         self.update_state(state_updates=algo_state_dict,device_list=device_list,shm_list = shm_list)
 
-        logging.info(self.ipca_dict)
         with TaskTimer(self.task_durations, "Initializing model"):
             ipca = IncrementalPCAonGPU(n_components = self.num_components, batch_size = self.batch_size, device = device, state_dict = ipca_state_dict)
 
