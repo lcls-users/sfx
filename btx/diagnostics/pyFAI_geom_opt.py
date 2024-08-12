2f4fb3b52ccc9a38b18b5f508cdea14e316fb198
LouConreux <louis.conreux1@gmail.com>
2024-08-12 00:49:03 -0700

    [edit] Fix case in Cross Entropy when no control points extracted

--- btx/diagnostics/pyFAI_geom_opt.py
+++ btx/diagnostics/pyFAI_geom_opt.py
@@ -734,7 +734,7 @@ class CrossEntropyGeomOpt:
                 geom_initial = pyFAI.geometry.Geometry(dist=dist, poni1=poni1, poni2=poni2, rot1=rot1, rot2=rot2, rot3=rot3, detector=self.detector, wavelength=wavelength)
                 sg = SingleGeometry("extract_cp", powder_img, calibrant=calibrant, detector=self.detector, geometry=geom_initial)
                 sg.extract_cp(max_rings=5, pts_per_deg=1, Imin=8*photon_energy)
-                if len(sg.control_points) == 0:
+                if len(sg.geometry_refinement.data) == 0:
                     score = np.inf
                 else:
                     score = sg.geometry_refinement.refine3(fix=fix)
