38d6e4cb9366de13487de68acd47ffcc25f031b9
LouConreux <louis.conreux1@gmail.com>
2024-09-03 02:46:32 -0700

    [test] beta=0.1, length=0.1

--- btx/diagnostics/pyFAI_geom_opt.py
+++ btx/diagnostics/pyFAI_geom_opt.py
@@ -528,7 +528,7 @@ class BayesGeomOpt:
         visited_idx = list(idx_samples.flatten())
 
         if af == "ucb":
-            beta = 2
+            beta = 0.1
             af = self.upper_confidence_bound
         elif af == "ei":
             epsilon = 0
