a4355a9d9949b7a689a58c5870814aeaaaa9bfd5
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-09-04 18:17:19 +0200

    Added new task

--- btx/misc/t_snes.py
+++ btx/misc/t_snes.py
@@ -104,7 +104,7 @@ def process(rank, imgs, V, S, num_images,device_list):
     print(f"Projectors on GPU {rank} computed",flush=True)
     U = U.cpu().detach().numpy()
     U = np.array([u.flatten() for u in U]) ##
-    tsne = TSNE(n_components=2, perplexity=50, n_neighbors=32)
+    tsne = TSNE(n_components=2, perplexity=50, n_neighbors=32, method='barnes_hut')
     embedding = tsne.fit_transform(U)
     trustworthiness_score = cuml_trustworthiness(U, embedding)
 
