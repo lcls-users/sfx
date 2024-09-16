e8f475e1a7a341308acd583b9617b842ae9e540b
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-09-16 10:17:02 +0200

    Changed loading nmber

--- btx/interfaces/iclient.py
+++ btx/interfaces/iclient.py
@@ -344,11 +344,11 @@ if __name__ == "__main__":
 
                     
                     for batch in dataloader_iter:
+                        current_loading_batch.append(batch)
+                        num_images_seen += len(batch)
                         if num_images_seen + len(current_loading_batch) > num_training_images + start_offset and current_loading_batch != []:
                             last_batch = True
                             break
-                        current_loading_batch.append(batch)
-                        num_images_seen += len(batch)
 
                     intermediate_time = time.time()
                     l_time += intermediate_time-beginning_time
