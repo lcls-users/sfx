14e7a60c7c90b239f213c163437024a24722eb4d
LouConreux <louis.conreux1@gmail.com>
2024-09-18 12:32:35 -0700

    [edit] Change covariance matrix def for prior distribution so that 99.7% (3 sigma) of sampled inputs are within the bounds

--- btx/diagnostics/pyFAI_geom_opt.py
+++ btx/diagnostics/pyFAI_geom_opt.py
@@ -506,7 +506,7 @@ class BayesGeomOpt:
         if prior:
             print("Using prior information...")
             means = np.mean(X_space, axis=0)
-            cov = np.diag([np.std(input_range[param])**2 / 2 for param in self.param_space])
+            cov = np.diag([((bounds[param][1] - bounds[param][0]) / 3)**2 for param in self.param_space])
             X_samples = np.random.multivariate_normal(means, cov, n_samples)
             X_norm_samples = (X_samples - np.mean(X_space, axis=0)) / (np.max(X_space, axis=0) - np.min(X_space, axis=0))
             for param in self.param_order:
@@ -514,6 +514,7 @@ class BayesGeomOpt:
                     idx = self.param_order.index(param)
                     X_samples = np.insert(X_samples, idx, values[param], axis=1)
         else:
+            print("Randomly picking samples...")
             idx_samples = np.random.choice(X.shape[0], n_samples)
             X_samples = X[idx_samples]
             X_norm_samples = X_norm[idx_samples]
