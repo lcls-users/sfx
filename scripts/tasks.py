1d419ccd49f0381a628966cee38cc3bc781d7d3e
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-10-07 14:34:50 -0700

    Test new scheduler

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -842,6 +842,7 @@ def create_pypca(config):
     comm = MPI.COMM_WORLD
     ncores = comm.Get_size()
     compute_projected_images = task.compute_projected_images
+    num_nodes = 2
 
     if task.get('loading_batch_size') is not None:
         loading_batch_size = task.loading_batch_size
@@ -859,7 +860,7 @@ def create_pypca(config):
     command += "; conda activate /sdf/group/lcls/ds/tools/conda_envs/py3.11-nopsana-torch-rapids; which python; echo 'Client environment activated'"
     command += f"; python {client_path} -e {exp} -r {run} -d {det_type} --start_offset {start_offset} --num_images '{num_images_str}' --loading_batch_size {loading_batch_size} --num_components {num_components} --batch_size {batch_size} --path {path} --tag {tag} --training_percentage {training_percentage} --smoothing_function {smoothing_function} --num_gpus {num_gpus} --compute_loss {compute_loss} --num_runs {num_runs} --compute_projected_images {compute_projected_images}"
 
-    js = JobScheduler(os.path.join(".", f'create_pypca_{num_components}_{num_tot_images}_{batch_size}.sh'),queue = 'ampere', ncores=  1, jobname=f'create_pypca_{num_components}_{num_tot_images}_{batch_size}',logdir='/sdf/home/n/nathfrn/btx/scripts',account='lcls',mem = '200G',num_gpus = num_gpus)
+    js = JobScheduler(os.path.join(".", f'create_pypca_{num_components}_{num_tot_images}_{batch_size}.sh'),queue = 'ampere', ncores=  1, jobname=f'create_pypca_{num_components}_{num_tot_images}_{batch_size}',logdir='/sdf/home/n/nathfrn/btx/scripts',account='lcls',mem = '200G',num_gpus = num_gpus, num_nodes = num_nodes)
     js.write_header()
     js.write_main(f"{command}\n", dependencies=['psana'],find_python_path=False)
     js.clean_up()
