6f89ed3fa93be83b9e3051feecf703c3afd9aa00
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-10-07 17:43:08 -0700

    Testy

--- btx/interfaces/iclient.py
+++ btx/interfaces/iclient.py
@@ -400,7 +400,7 @@ if __name__ == "__main__":
                     current_loading_batch = np.split(current_loading_batch, num_tot_gpus,axis=1)
                     current_loading_batch = current_loading_batch[id_current_node*num_gpus:(id_current_node+1)*num_gpus]
 
-                    print("Shape current batch:",current_loading_batch.shape,flush=True)
+                    print("Shape current batch:",len(current_loading_batch),current_loading_batch[0].shape,flush=True)
                     shape = current_loading_batch[0].shape
                     dtype = current_loading_batch[0].dtype
 
@@ -495,7 +495,7 @@ if __name__ == "__main__":
                         current_loading_batch = mapping_function(current_loading_batch, type_mapping = smoothing_function)
                         current_loading_batch = np.split(current_loading_batch, num_tot_gpus,axis=1)
                         current_loading_batch = current_loading_batch[id_current_node*num_gpus:(id_current_node+1)*num_gpus]
-
+                        
                         shape = current_loading_batch[0].shape
                         dtype = current_loading_batch[0].dtype
 
