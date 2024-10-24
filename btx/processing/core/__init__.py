a621d5f32cc759d13fb15907223aa83cb610c3de
O H <ohoidn@slac.stanford.edu>
2024-10-24 16:42:14 -0400

    feat: Add btx/processing/core/__init__.py

---
+++ btx/processing/core/__init__.py
@@ -0,0 +1,25 @@
+# btx/processing/core/__init__.py
+"""Core pipeline framework components."""
+
+# Import and expose all core components
+from .task import Task
+from .pipeline import Pipeline, PipelineResult, TaskResult
+from .builders import PipelineBuilder
+from .adapters import (
+    TaskAdapter, TaskRegistry, register_task,
+    registry as task_registry,
+    AdapterValidationError
+)
+
+__all__ = [
+    'Task',
+    'Pipeline',
+    'PipelineResult',
+    'TaskResult',
+    'PipelineBuilder',
+    'TaskAdapter',
+    'TaskRegistry',
+    'register_task',
+    'task_registry',
+    'AdapterValidationError'
+]
