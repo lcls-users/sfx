0ce56c9fa584feb946b5d1c1f15d990583708253
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-10-30 15:29:05 -0700

    Test

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -1036,9 +1036,9 @@ def reduce_pypca_multinodes(config):
             args = [(config, num_nodes, node) for node in range(num_nodes)]
             pool.starmap(reduce_pypca, args)
         algo_end_time = time.time()
-        print(f"Algorithm time: {algo_end_time - algo_start_time}")
+        print(f"Algorithm time: {algo_end_time - algo_start_time}")"""
         
-        model_path = os.path.dirname(config.reduce_pypca_multinodes.model)"""
+        model_path = os.path.dirname(config.reduce_pypca_multinodes.model)
         tag = f"projected_images_{config.setup.exp}_start_run_{config.reduce_pypca_multinodes.run}_num_images_{config.reduce_pypca_multinodes.num_images}"
         clean_pypca(model_path, tag, num_nodes,mode='reduce')
 
