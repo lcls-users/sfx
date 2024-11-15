06b1683582ac9910e964c57c7f97573c3540a463
Nathan Fournier <nathan.fournier@student-cs.fr>
2024-11-15 15:04:15 -0800

    Test

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -817,7 +817,7 @@ def t_sne(config):
     command += "; conda activate /sdf/group/lcls/ds/tools/conda_envs/py3.11-nopsana-torch-rapids; which python; echo 'Client environment activated'"
     command += f"; python {client_path} --filename {filename} --num_images {num_images_str} --loading_batch_size {loading_batch_size} --num_tries {num_tries} --threshold {threshold} --num_runs{num_runs}"
 
-    js = JobScheduler(os.path.join(".", f't_snes_{sum(num_images)}.sh'),queue = 'ampere', ncores=  1, jobname=f't_snes_{sum(num_images)}',logdir='/sdf/home/n/nathfrn/btx/scripts',account='lcls',mem = '200G',num_gpus = 4) ##
+    js = JobScheduler(os.path.join(".", f't_snes_{sum(num_images_str)}.sh'),queue = 'ampere', ncores=  1, jobname=f't_snes_{sum(num_images_str)}',logdir='/sdf/home/n/nathfrn/btx/scripts',account='lcls',mem = '200G',num_gpus = 4) ##
     js.write_header()
     js.write_main(f"{command}\n", dependencies=['psana'],find_python_path=False)
     js.clean_up()
